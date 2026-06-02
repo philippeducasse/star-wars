c3647815717844b92ef6d70131763602011abbf7
ducasse@vdpresearch.de <philippe@NBPD082025.vdpGruppe.local>
2026-06-02 14:56:02 +0200

    feat: add star wars logo to navbar

--- src/components/generic/navigation/Navbar.tsx
+++ src/components/generic/navigation/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-
+import Image from "next/image";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -12,19 +12,40 @@ import {
 
 import { capitalize } from "@/lib/utils";
 
-const components: string[] = ["characters", "planets", "films", "species", "vehicles", "starships"];
+const components: string[] = [
+  "characters",
+  "planets",
+  "films",
+  "logo",
+  "species",
+  "vehicles",
+  "starships",
+];
 
 export function Navbar() {
   return (
-    <NavigationMenu>
+    <NavigationMenu className="mx-auto">
       <NavigationMenuList>
-        {components.map((component, idx) => (
-          <NavigationMenuItem key={`${component}_${idx}`}>
-            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-              <Link href={component}>{capitalize(component)}</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
+        {components.map((component, idx) =>
+          component === "logo" ? (
+            <Image
+              src={"sw_logo.svg"}
+              key={"logo"}
+              alt="star wars classic logo"
+              width={150}
+              height={50}
+              className="mx-6"
+            />
+          ) : (
+            <NavigationMenuItem key={`${component}_${idx}`}>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link href={component}>
+                  <p className="text-2xl text-sw-yellow">{capitalize(component)}</p>
+                </Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ),
+        )}
       </NavigationMenuList>
     </NavigationMenu>
   );
